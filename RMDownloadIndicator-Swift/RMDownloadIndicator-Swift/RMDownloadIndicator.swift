073688bdbb91c911df94caa4dc37dc7cd93128dc
Mahesh Shanbhag <ShanbhagMahesh@Maheshs-MacBook-Pro.local>
2015-08-11 17:43:00 +0530

    Adding swift support.

---
+++ RMDownloadIndicator-Swift/RMDownloadIndicator-Swift/RMDownloadIndicator.swift
@@ -0,0 +1,241 @@
+//
+//  RMDownloadIndicator.swift
+//  RMDownloadIndicator-Swift
+//
+//  Created by Mahesh Shanbhag on 10/08/15.
+//  Copyright (c) 2015 Mahesh Shanbhag. All rights reserved.
+//
+
+import UIKit
+
+enum RMIndicatorType: Int {
+    case kRMClosedIndicator = 0
+    case kRMFilledIndicator
+    case kRMMixedIndictor
+}
+
+class RMDownloadIndicator: UIView {
+    
+    // this value should be 0 to 0.5 (default: (kRMFilledIndicator = 0.5), (kRMMixedIndictor = 0.4))
+    var radiusPercent: CGFloat = 0.5 {
+        didSet {
+            if type == RMIndicatorType.kRMClosedIndicator {
+                self.radiusPercent = 0.5
+            }
+            if radiusPercent > 0.5 || radiusPercent < 0 {
+                radiusPercent = oldValue
+            }
+        }
+    }
+    
+    // used to fill the downloaded percent slice (default: (kRMFilledIndicator = white), (kRMMixedIndictor = white))
+    var fillColor: UIColor = UIColor.clearColor() {
+        didSet {
+            if type == RMIndicatorType.kRMClosedIndicator {
+                fillColor = UIColor.clearColor()
+            }
+        }
+    }
+    
+    // used to stroke the covering slice (default: (kRMClosedIndicator = white), (kRMMixedIndictor = white))
+    var strokeColor: UIColor = UIColor.whiteColor()
+    
+    // used to stroke the background path the covering slice (default: (kRMClosedIndicator = gray))
+    var closedIndicatorBackgroundStrokeColor: UIColor = UIColor.whiteColor()
+    
+    
+    // Private properties
+    private var paths: [CGPath] = []
+    private var indicateShapeLayer: CAShapeLayer!
+    private var coverLayer: CAShapeLayer!
+    private var animatingLayer: CAShapeLayer!
+    private var type: RMIndicatorType!
+    private var coverWidth: CGFloat = 0.0
+    private var lastUpdatedPath: UIBezierPath!
+    private var lastSourceAngle: CGFloat = 0.0
+    private var animationDuration: CGFloat = 0.0
+    
+    
+    
+    // init with frame and type
+    // if() - (id)initWithFrame:(CGRect)frame is used the default type = kRMFilledIndicator
+    override init(frame:CGRect) {
+        super.init(frame:frame)
+        
+        self.type = .kRMFilledIndicator
+        self.initAttributes()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(rectframe: CGRect, type: RMIndicatorType) {
+        super.init(frame: rectframe)
+        
+        self.type = type
+        self.initAttributes()
+    }
+    
+    func initAttributes() {
+        if type == RMIndicatorType.kRMClosedIndicator {
+            self.radiusPercent = 0.5
+            coverLayer = CAShapeLayer()
+            animatingLayer = coverLayer
+            fillColor = UIColor.clearColor()
+            strokeColor = UIColor.whiteColor()
+            closedIndicatorBackgroundStrokeColor = UIColor.grayColor()
+            coverWidth = 2.0
+        }
+        else {
+            if type == RMIndicatorType.kRMFilledIndicator {
+                indicateShapeLayer = CAShapeLayer()
+                animatingLayer = indicateShapeLayer
+                radiusPercent = 0.5
+                coverWidth = 2.0
+                closedIndicatorBackgroundStrokeColor = UIColor.clearColor()
+            }
+            else {
+                indicateShapeLayer = CAShapeLayer()
+                coverLayer = CAShapeLayer()
+                animatingLayer = indicateShapeLayer
+                coverWidth = 2.0
+                radiusPercent = 0.4
+                closedIndicatorBackgroundStrokeColor = UIColor.whiteColor()
+            }
+            fillColor = UIColor.whiteColor()
+            strokeColor = UIColor.whiteColor()
+        }
+        
+        animatingLayer.frame = self.bounds
+        self.layer.addSublayer(animatingLayer)
+        animationDuration = 0.5
+    }
+    
+    // prepare the download indicator
+    func loadIndicator() {
+        var center: CGPoint = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)
+        var initialPath: UIBezierPath = UIBezierPath.init()
+        if type == RMIndicatorType.kRMClosedIndicator {
+            initialPath.addArcWithCenter(center, radius: (fmin(CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds))), startAngle: degreeToRadian(-90), endAngle: degreeToRadian(-90), clockwise: true)
+        }
+        else {
+            if type == RMIndicatorType.kRMMixedIndictor {
+                self.setNeedsDisplay()
+            }
+            var radius: CGFloat = (fmin(CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds)) / 2) * self.radiusPercent
+            initialPath.addArcWithCenter(center, radius: radius, startAngle: degreeToRadian(-90), endAngle: degreeToRadian(-90), clockwise: true)
+        }
+        animatingLayer.path = initialPath.CGPath
+        animatingLayer.strokeColor = strokeColor.CGColor
+        animatingLayer.fillColor = fillColor.CGColor
+        animatingLayer.lineWidth = coverWidth
+        self.lastSourceAngle = degreeToRadian(-90)
+    }
+    
+    func keyframePathsWithDuration(duration: CGFloat, lastUpdatedAngle: CGFloat, newAngle: CGFloat, radius: CGFloat, type: RMIndicatorType) -> [CGPath] {
+        var frameCount: Int = Int(ceil(duration * 60))
+        var array: [CGPath] = []
+        for var frame = 0; frame <= frameCount; frame++ {
+            var startAngle = degreeToRadian(-90)
+            
+            var angleChange = ((newAngle - lastUpdatedAngle) * CGFloat(frame))
+            var endAngle = lastUpdatedAngle + (angleChange / CGFloat(frameCount))
+            array.append((self.pathWithStartAngle(startAngle, endAngle: endAngle, radius: radius, type: type).CGPath))
+        }
+        return array
+    }
+    
+    func pathWithStartAngle(startAngle: CGFloat, endAngle: CGFloat, radius: CGFloat, type: RMIndicatorType) -> UIBezierPath {
+        var clockwise: Bool = startAngle < endAngle
+        var path: UIBezierPath = UIBezierPath()
+        var center: CGPoint = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)
+        if type == RMIndicatorType.kRMClosedIndicator {
+            path.addArcWithCenter(center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        }
+        else {
+            path.moveToPoint(center)
+            path.addArcWithCenter(center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+            path.closePath()
+        }
+        return path
+    }
+    
+    override func drawRect(rect: CGRect) {
+        if type == RMIndicatorType.kRMMixedIndictor {
+            var radius: CGFloat = fmin(CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds)) / 2 - self.coverWidth
+            var center: CGPoint = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)
+            var coverPath: UIBezierPath = UIBezierPath()
+            coverPath.lineWidth = coverWidth
+            coverPath.addArcWithCenter(center, radius: radius, startAngle: CGFloat(0), endAngle: CGFloat(2 * M_PI), clockwise: true)
+            closedIndicatorBackgroundStrokeColor.set()
+            coverPath.stroke()
+        }
+        else {
+            if type == RMIndicatorType.kRMClosedIndicator {
+                var radius: CGFloat = (fmin(CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds)) / 2) - self.coverWidth
+                var center: CGPoint = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2)
+                var coverPath: UIBezierPath = UIBezierPath()
+                coverPath.lineWidth = coverWidth
+                coverPath.addArcWithCenter(center, radius: radius, startAngle: CGFloat(0), endAngle: CGFloat(2 * M_PI), clockwise: true)
+                closedIndicatorBackgroundStrokeColor.set()
+                coverPath.lineWidth = self.coverWidth
+                coverPath.stroke()
+            }
+        }
+    }
+    
+    // update the downloadIndicator
+    func updateWithTotalBytes(bytes: CGFloat, downloadedBytes: CGFloat) {
+        lastUpdatedPath = UIBezierPath.init(CGPath: animatingLayer.path)
+        paths.removeAll(keepCapacity: false)
+        var destinationAngle: CGFloat = self.destinationAngleForRatio((downloadedBytes / bytes))
+        var radius: CGFloat = (fmin(CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds)) * radiusPercent) - self.coverWidth
+        paths = self.keyframePathsWithDuration(self.animationDuration, lastUpdatedAngle: self.lastSourceAngle, newAngle: destinationAngle, radius: radius, type: type)
+        animatingLayer.path = paths[(paths.count - 1)]
+        self.lastSourceAngle = destinationAngle
+        var pathAnimation: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "path")
+        pathAnimation.values = paths
+        pathAnimation.duration = CFTimeInterval(animationDuration)
+        pathAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        pathAnimation.removedOnCompletion = true
+        animatingLayer.addAnimation(pathAnimation, forKey: "path")
+    }
+    
+    func destinationAngleForRatio(ratio: CGFloat) -> CGFloat {
+        return (degreeToRadian((360 * ratio) - 90))
+    }
+    
+    
+    func degreeToRadian(degree: CGFloat) -> CGFloat
+    {
+        return (CGFloat(degree) * CGFloat(M_PI)) / CGFloat(180.0);
+    }
+//    
+//    func setLayerFillColor(fillColor: UIColor) {
+//        if type == RMIndicatorType.kRMClosedIndicator {
+//            self.fillColor = UIColor.clearColor()
+//        }
+//        else {
+//            self.fillColor = fillColor
+//        }
+//    }
+    
+//    func setLayerRadiusPercent(radiusPercent: CGFloat) {
+//        if type == RMIndicatorType.kRMClosedIndicator {
+//            self.radiusPercent = 0.5
+//            return
+//        }
+//        if radiusPercent > 0.5 || radiusPercent < 0 {
+//            return
+//        }
+//        else {
+//            self.radiusPercent = radiusPercent
+//        }
+//    }
+    
+    // update the downloadIndicator
+    func setIndicatorAnimationDuration(duration: CGFloat) {
+        self.animationDuration = duration
+    }
+}
---
+++ RMDownloadIndicator-Swift/RMDownloadIndicator-Swift/ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  RMDownloadIndicator-Swift
+//
+//  Created by Mahesh Shanbhag on 10/08/15.
+//  Copyright (c) 2015 Mahesh Shanbhag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var animationSwitch: UISwitch!
+    var closedIndicator: RMDownloadIndicator!
+    var filledIndicator: RMDownloadIndicator!
+    var mixedIndicator: RMDownloadIndicator!
+
+    var downloadedBytes: CGFloat = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.settingChanged(self.animationSwitch)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func addDownloadIndicators() {
+        
+        if self.closedIndicator != nil {
+            self.closedIndicator.removeFromSuperview()
+            self.closedIndicator = nil
+        }
+        
+        if self.filledIndicator != nil {
+            self.filledIndicator.removeFromSuperview()
+            self.filledIndicator = nil
+        }
+        
+        if self.mixedIndicator != nil {
+            self.mixedIndicator.removeFromSuperview()
+            self.mixedIndicator = nil
+        }
+        
+        var closedIndicator: RMDownloadIndicator = RMDownloadIndicator(rectframe: CGRectMake((CGRectGetWidth(self.view.bounds) - 80) / 2, CGRectGetMaxY(self.animationSwitch.frame) + 60.0, 80, 80), type: RMIndicatorType.kRMClosedIndicator)
+        closedIndicator.backgroundColor = UIColor.whiteColor()
+        closedIndicator.fillColor = UIColor(red: 16/255, green: 119/255, blue: 234/255, alpha: 1.0)
+        closedIndicator.strokeColor = UIColor(red:16/255, green: 119/255, blue: 234/255, alpha: 1.0)
+        closedIndicator.radiusPercent = 0.45
+        self.view.addSubview(closedIndicator)
+        closedIndicator.loadIndicator()
+        self.closedIndicator = closedIndicator
+        
+        var filledIndicator: RMDownloadIndicator = RMDownloadIndicator(rectframe: CGRectMake((CGRectGetWidth(self.view.bounds) - 80) / 2, CGRectGetMaxY(self.closedIndicator.frame) + 40.0, 80, 80), type: RMIndicatorType.kRMFilledIndicator)
+        filledIndicator.backgroundColor = UIColor.whiteColor()
+        filledIndicator.fillColor = UIColor(red: 16.0/255, green: 119.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        filledIndicator.strokeColor = UIColor(red: 16.0/255.0, green: 119.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        filledIndicator.radiusPercent = 0.45
+        self.view.addSubview(filledIndicator)
+        filledIndicator.loadIndicator()
+        self.filledIndicator = filledIndicator
+        
+        var mixedIndicator: RMDownloadIndicator = RMDownloadIndicator(rectframe: CGRectMake((CGRectGetWidth(self.view.bounds) - 80) / 2, CGRectGetMaxY(self.filledIndicator.frame) + 40.0, 80, 80), type: RMIndicatorType.kRMMixedIndictor)
+        mixedIndicator.backgroundColor = UIColor.whiteColor()
+        mixedIndicator.fillColor = UIColor(red: 16.0/255.0, green: 119.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        mixedIndicator.strokeColor = UIColor(red: 16.0/255.0, green: 119.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        mixedIndicator.closedIndicatorBackgroundStrokeColor = UIColor(red:16.0/255.0, green: 119.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        mixedIndicator.radiusPercent = 0.45
+        self.view.addSubview(mixedIndicator)
+        mixedIndicator.loadIndicator()
+        self.mixedIndicator = mixedIndicator
+    }
+    
+    func startAnimation() {
+        self.addDownloadIndicators()
+        if !animationSwitch.on {
+            self.updateViewOneTime()
+            return
+        }
+        
+        self.downloadedBytes = 0
+        self.animationSwitch.userInteractionEnabled = false
+        
+        var delayInSeconds: Int64 = 1
+        var popTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Int64(NSEC_PER_SEC)))
+        dispatch_after(popTime, dispatch_get_main_queue(), {() in        self.updateView(10.0)
+            
+        })
+        var delayInSeconds1: Int64 = delayInSeconds + 1
+        var popTime1: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds1 * Int64(NSEC_PER_SEC)))
+        dispatch_after(popTime1, dispatch_get_main_queue(), {() in        self.updateView(30.0)
+            
+        })
+        var delayInSeconds2: Int64 = delayInSeconds1 + 1
+        var popTime2: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds2 * Int64(NSEC_PER_SEC)))
+        dispatch_after(popTime2, dispatch_get_main_queue(), {() in        self.updateView(10.0)
+            
+        })
+        var delayInSeconds3: Int64 = delayInSeconds2 + 1
+        var popTime3: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds3 * Int64(NSEC_PER_SEC)))
+        dispatch_after(popTime3, dispatch_get_main_queue(), {() in        self.updateView(50.0)
+            self.animationSwitch.userInteractionEnabled = true
+            
+        })
+    }
+    
+    func updateView(val: CGFloat) {
+        self.downloadedBytes += val
+        closedIndicator.updateWithTotalBytes(100, downloadedBytes: self.downloadedBytes)
+        filledIndicator.updateWithTotalBytes(100, downloadedBytes: self.downloadedBytes)
+        mixedIndicator.updateWithTotalBytes(100, downloadedBytes: self.downloadedBytes)
+    }
+    
+    func updateViewOneTime() {
+        closedIndicator.setIndicatorAnimationDuration(1.0)
+        filledIndicator.setIndicatorAnimationDuration(1.0)
+        mixedIndicator.setIndicatorAnimationDuration(1.0)
+        closedIndicator.updateWithTotalBytes(100, downloadedBytes: 100)
+        filledIndicator.updateWithTotalBytes(100, downloadedBytes: self.downloadedBytes)
+        mixedIndicator.updateWithTotalBytes(100, downloadedBytes: self.downloadedBytes)
+    }
+    
+    @IBAction func settingChanged(sender: UISwitch) {
+        if self.animationSwitch.on {
+            //self.settings.setText("Multi Time Animation")
+            self.startAnimation()
+        }
+        else {
+            //self.settings.setText("One Time Animation")
+            self.startAnimation()
+        }
+    }
+
+
+}
+
